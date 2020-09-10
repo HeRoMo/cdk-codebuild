b8fb4251d985d8762a7ba8e5c0d9c772cea960a3
HeRoMo <hero@asterisk-works.jp>
2020-09-10 19:10:59 +0900

    Initial commit

---
+++ test/cdk-codebuild.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as CdkCodebuild from '../lib/cdk-codebuild-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkCodebuild.CdkCodebuildStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
