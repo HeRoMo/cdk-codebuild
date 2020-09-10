30403e92237472d6260078d9d6566b7eb9ec1df8
HeRoMo <hero@asterisk-works.jp>
2020-09-10 19:23:07 +0900

    add eslint

--- test/cdk-codebuild.test.ts
+++ test/cdk-codebuild.test.ts
@@ -3,11 +3,11 @@ import * as cdk from '@aws-cdk/core';
 import * as CdkCodebuild from '../lib/cdk-codebuild-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkCodebuild.CdkCodebuildStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkCodebuild.CdkCodebuildStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    Resources: {},
+  }, MatchStyle.EXACT));
 });
