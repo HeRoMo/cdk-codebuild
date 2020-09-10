3d35bb13dfcae52533aaa052d66d10e5e2572816
HeRoMo <hero@asterisk-works.jp>
2020-09-10 23:20:28 +0900

    add codebuild

--- lib/cdk-codebuild-stack.ts
+++ lib/cdk-codebuild-stack.ts
@@ -1,9 +1,35 @@
-import * as cdk from '@aws-cdk/core';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import {
+  Project, BuildSpec,
+  // EventAction, FilterGroup,
+  Source,
+} from '@aws-cdk/aws-codebuild';
 
-export class CdkCodebuildStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CdkCodebuildStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const gitHubSource = Source.gitHub({
+      owner: 'HeRoMo',
+      repo: 'cdk-codebuild',
+      webhook: false, // optional, default: true if `webhookFilters` were provided, false otherwise
+      // webhookFilters: [
+      //   FilterGroup.inEventOf(EventAction.PUSH).andBranchIs('master'),
+      // ], // optional, by default all pushes and Pull Requests will trigger a build
+    });
+
+    new Project(this, 'MyProject', {
+      source: gitHubSource,
+      buildSpec: BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'echo "Hello, CodeBuild!"',
+            ],
+          },
+        },
+      }),
+    });
   }
 }
