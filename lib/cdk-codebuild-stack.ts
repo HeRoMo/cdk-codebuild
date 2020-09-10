d1b42f24bad9d4778d38f72cb16fbc5f0b298d08
HeRoMo <hero@asterisk-works.jp>
2020-09-11 00:27:26 +0900

    modify project properties

--- lib/cdk-codebuild-stack.ts
+++ lib/cdk-codebuild-stack.ts
@@ -3,6 +3,8 @@ import {
   Project, BuildSpec,
   // EventAction, FilterGroup,
   Source,
+  LinuxBuildImage,
+  ComputeType,
 } from '@aws-cdk/aws-codebuild';
 
 export class CdkCodebuildStack extends Stack {
@@ -18,14 +20,24 @@ export class CdkCodebuildStack extends Stack {
       // ], // optional, by default all pushes and Pull Requests will trigger a build
     });
 
-    new Project(this, 'MyProject', {
+    const project = new Project(this, 'CdkCodeBUildSample', {
+      projectName: 'cdk-codebuild-sample',
       source: gitHubSource,
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_4_0,
+        computeType: ComputeType.SMALL,
+        privileged: true,
+      },
       buildSpec: BuildSpec.fromObject({
         version: '0.2',
+        env: {
+          shell: 'bash',
+        },
         phases: {
           build: {
             commands: [
               'echo "Hello, CodeBuild!"',
+              'codebuild-breakpoint',
             ],
           },
         },
